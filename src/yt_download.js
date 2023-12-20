9cfe6232fd1c81d486012d6784d097da27669f4f
Code-seven123 <manu024595@gmail.com>
2023-12-20 14:26:37 +0700

    update yt.js

--- src/yt_download.js
+++ src/yt_download.js
@@ -2,11 +2,11 @@ import { youtubedl } from '@bochilteam/scraper-sosmed'
 
 export default async (url) => {
   try {
-    const data = await youtubedl(yt)
+    const data = await youtubedl(url)
     const down = await data.video.auto.download()
     return {
       title: data.title,
-      thubnails: data.thubnail,
+      thubnails: data.thumbnail,
       url: down
     }
   } catch (e) {
